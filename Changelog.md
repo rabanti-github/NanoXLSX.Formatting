90e75f9cbecc0c6409204efd9d52a676e4293493
Raphael Stoeckli <github@rabanti.ch>
2022-10-07 13:40:12 +0200

    Initial commit

---
+++ Changelog.md
@@ -0,0 +1,6 @@
+# Change Log
+
+## v1.0.0
+
+---
+Release Date: **xx.xx.2022**
---
+++ NanoXLSX.Formatting/NanoXLSX.Formatting .NET Standard/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NanoXLSX.Formatting
+{
+    public class Class1
+    {
+        // DUMMY --> Use source link to .NET library
+    }
+}
